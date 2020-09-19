742e8e6e0b3f7454d90ed4faceac000e8afc4880
geny <genys@zivitech.com>
2020-09-19 03:10:04 +0300

    refactor add device

--- client/modules/AddDevice/AddDevice.js
+++ client/modules/AddDevice/AddDevice.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik } from 'formik'
 import { connect } from 'react-redux'
 import I18n from '$utils/I18n'
@@ -11,74 +11,53 @@ import api from '$api/device'
 
 import './addDevice.scss'
 
-class AddDevice extends React.Component {
-  state = {
-    isLoginFormActive: true,
-    loading: false,
-  }
-
-  showLoginForm = () => this.setState({ isLoginFormActive: true })
-
-  showRegistrationForm = () => this.setState({ isLoginFormActive: false })
-
-  addDevice = async (values, { setErrors }) => {
-    const { history } = this.props
-    this.setState({ loading: true })
+function AddDevice ({ history, authorized, match: { params: { uid } } }) {
+  const [loading, setLoading] = useState(false)
+  const [isLoginFormVisible, showLoginForm] = useState(false)
+  const addDevice = async (values, { setErrors }) => {
+    setLoading(true)
     try {
       const { success, payload } = await api.add(values)
-      if (!success) {
-        setErrors(payload)
-        return
-      }
+      if (!success) return setErrors(payload)
       history.replace('/devices')
     } catch (errors) {
-      this.setState({ loading: false })
+      setLoading(false)
     }
   }
-
-  render () {
-    const { authorized, match } = this.props
-    const { uid } = match.params
-    const { isLoginFormActive, loading } = this.state
-    return (
-      <div className="add-device">
-        {authorized ? (
-          <Formik
-            initialValues={{ deviceName: '', uid }}
-            validate={validateForm}
-            onSubmit={this.addDevice}
-          >
-            {({ handleSubmit, values, errors }) => (
-              <form className="form" onSubmit={handleSubmit}>
-                <Input name="deviceName" />
-                <div className="form__buttons">
-                  <Button
-                    modifiers="btn--primary"
-                    loading={loading}
-                    disabled={!checkIsFormValid(values, errors)}
-                  >
-                    <I18n t="buttons.add" />
-                  </Button>
-                </div>
-              </form>
-            )}
-          </Formik>
-        ) : (
-          <>
-            {isLoginFormActive ? (
-              <LoginForm
-                showRegistration={this.showRegistrationForm}
-              />
-            ) : (
-              <RegistrationForm
-                showLogin={this.showLoginForm}
-              />
-            )}
-          </>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div className="add-device">
+      {authorized ? (
+        <Formik
+          initialValues={{ deviceName: '', uid }}
+          validate={validateForm}
+          onSubmit={addDevice}
+        >
+          {({ handleSubmit, values, errors }) => (
+            <form className="form" onSubmit={handleSubmit}>
+              <Input name="deviceName" />
+              <div className="form__buttons">
+                <Button
+                  modifiers="btn--primary"
+                  loading={loading}
+                  disabled={!checkIsFormValid(values, errors)}
+                >
+                  <I18n t="buttons.add" />
+                </Button>
+              </div>
+            </form>
+          )}
+        </Formik>
+      ) : (
+        <>
+          {isLoginFormVisible ? (
+            <LoginForm showRegistration={() => showLoginForm(false)} />
+          ) : (
+            <RegistrationForm showLogin={() => showLoginForm(true)} />
+          )}
+        </>
+      )}
+    </div>
+  )
 }
 
 const mapStateToProps = ({ user }) => ({
