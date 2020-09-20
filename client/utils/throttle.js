b8e76efaf5e6981f6feeed0c04cad4f9be987d71
geny <genys@zivitech.com>
2020-09-20 23:16:17 +0300

    fix throttle

--- client/utils/throttle.js
+++ client/utils/throttle.js
@@ -1,18 +1,12 @@
 export default function throttle (func, time) {
   let inThrottle = false
-  let timeouId = null
   return function (...args) {
     const context = this
-    clearTimeout(timeouId)
-    timeouId = setTimeout(() => {
+    if (inThrottle) return
+    inThrottle = true
+    func.apply(context, args)
+    setTimeout(() => {
       inThrottle = false
-      func.apply(context, args)
     }, time)
-    if (!inThrottle) {
-      inThrottle = true
-      setTimeout(() => {
-        inThrottle = false
-      }, time)
-    }
   }
 }
